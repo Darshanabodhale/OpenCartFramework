6c128aef35ce19917bdfae0e0cf23ae8d1a03661
Darshana <darshanabodhale9@gmail.com>
2026-03-26 18:01:30 +0530

    Third commit

--- src/test/java/testCases/TC02_LoginTest.java
+++ src/test/java/testCases/TC02_LoginTest.java
@@ -9,8 +9,40 @@ import pageObjects.LoginPage;
 import pageObjects.MyAccountPage;
 import testBase.BaseClass;
 
-public class TC02_LoginTest extends BaseClass{
+	public class TC02_LoginTest extends BaseClass{
 
-	//Implementation afterwards
+		@Test(groups= {"Sanity", "Master"})
+		public void verify_Login() {
+			logger.info("******* Starting TC02_LoginTest *******");
+			
+			try {
+				
+		
+			//HomePage
+			HomePage hp = new HomePage(driver);
+			
+			hp.clickMyAccount();
+			hp.clickLogin();
+			
+			//LoginPage
+			LoginPage lp = new LoginPage(driver);
+			
+			lp.setEmail(prop.getProperty("email"));
+			lp.setPassword(prop.getProperty("password"));
+			lp.clickLoginbtn();
+			
+			//MyAccountPage
+			MyAccountPage ma = new MyAccountPage(driver);
+			boolean targetPage = ma.isMyAccountPageExists();
+			
+			Assert.assertEquals(targetPage, true, "Login Failed");
+			}catch(Exception e) {
+				Assert.fail();
+			}
+			logger.info("******* Finished TC02_LoginTest *******");
+		}
+		
+		
+	}
 	
-}
+
